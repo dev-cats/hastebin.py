7a49511e0787fe5cd0e2b614d53edcba33b71e28
Miner ChAI <43097806+MinerChAI@users.noreply.github.com>
2019-04-22 21:20:21 +0300

    Fixed inconsistent quotes

--- hastebin/async_hastebin.py
+++ hastebin/async_hastebin.py
@@ -4,4 +4,4 @@ from aiohttp import ClientSession
 async def post(content, url='https://hastebin.com'):
     async with ClientSession() as session:
         async with session.post(f'{url}/documents', data=content.encode('utf-8')) as post:
-            return f'{url}/{(await post.json())["key"]}'
+            return url + '/' + (await post.json())['key']
--- hastebin/hastebin.py
+++ hastebin/hastebin.py
@@ -3,4 +3,4 @@ from requests import post
 
 def post(content, url='https://hastebin.com'):
     post = post(f'{url}/documents', data=content.encode('utf-8'))
-    return f'{url}/{post.json()["key"]}'
+    return url + '/' + post.json()['key']
